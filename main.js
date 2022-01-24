cde31569ddbfad5f733995bf70c5293dec99b2d6
Cadar <cadar.gabriel20@gmail.com>
2022-01-25 00:21:10 +0100

    Esercizio Completato

---
+++ main.js
@@ -0,0 +1,63 @@
+const pricePerKm = 0.21;
+const discountUnderAge = 0.2;
+const discountOver65 = 0.4;
+
+let namePassenger = document.getElementById("namePassenger");
+let agePassenger = document.getElementById("select");
+let showTicket = document.getElementById("js");
+let kmToRun = document.getElementById("kmToRun");
+
+let processData = document.getElementById("process-data");
+let discardData = document.getElementById("discard-data");
+
+let min = 90000;
+let max = 99999;
+let randomCpCode = Math.floor(Math.random() * (max - min) + min);
+
+let randomCoach = Math.floor(Math.random() * 9) + 1;
+
+processData.addEventListener("click",
+
+    function() {  
+
+        if (namePassenger.value == "" || kmToRun.value == "") {
+            alert("Inserisci un valore corretto per tutte le sezioni");
+        } else {
+            showTicket.classList.add("visible"); 
+
+            document.getElementById("traveller-name").innerHTML = namePassenger.value;
+            document.getElementById("coach").innerHTML = randomCoach;
+            document.getElementById("CP-code").innerHTML = randomCpCode;
+
+            let priceTicket = kmToRun.value * pricePerKm;
+            let priceDiscounted18 = priceTicket - (priceTicket * discountUnderAge);
+            let priceDiscounted65 = priceTicket - (priceTicket * discountOver65);
+
+            if (agePassenger.value == "Minorenne") {
+                document.getElementById("price").innerHTML = priceDiscounted18.toFixed(2) + "€";
+                document.getElementById("ticket-type").innerHTML = "Biglietto Under18";
+            } else if (agePassenger.value == "Over65") {
+                document.getElementById("price").innerHTML = priceDiscounted65.toFixed(2) + "€";
+                document.getElementById("ticket-type").innerHTML = "Biglietto Over65";
+            } else {
+                document.getElementById("price").innerHTML = priceTicket.toFixed(2) + "€";
+                document.getElementById("ticket-type").innerHTML = "Biglietto Standard";
+            }
+        } 
+    }
+);
+
+discardData.addEventListener("click",
+
+    function() {
+
+        showTicket.classList.remove("visible");
+
+        namePassenger.value = "";
+        kmToRun.value = "";
+
+        discardData = window.location.reload();
+
+    }
+
+)
